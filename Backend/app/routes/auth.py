9bdb8dcc7e815ff5e7228099a138831e41dd9693
Shangwee <hoshangwee0911@gmail.com>
2024-12-16 12:26:34 +0800

    Refactor auth codes

--- Backend/app/routes/auth.py
+++ Backend/app/routes/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, get_jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.models.user import User
