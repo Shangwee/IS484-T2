0f96019b524cbcb17e8e49d424899c38ae1ebc0c
Shangwee <hoshangwee0911@gmail.com>
2025-02-15 10:24:15 +0800

    changing it to an env variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://20.6.4.50:5001";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
